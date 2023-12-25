9575b7249abd27675a7dd03556d3e9ce076abe28
usuless <61857692+usuless@users.noreply.github.com>
2023-12-26 00:46:20 +0100

    fuckit

---
+++ game.js
@@ -0,0 +1,13 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+function nextSequence() {
+
+  var randomNumber = Math.floor(Math.random() * 4);
+
+  var randomChosenColour = buttonColours[randomNumber];
+
+  gamePattern.push(randomChosenColour);
+
+}
